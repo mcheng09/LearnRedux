0c404156ac5e82d457541aa8add6e027f761e876
Michael Cheng <mcheng.oc@gmail.com>
2016-10-27 21:09:58 -0700

    first commit from rBP3

---
+++ pure-function-example.js
@@ -0,0 +1,39 @@
+var redux = require('redux');
+
+console.log('Starting redux example')
+
+// Pure function
+function add(a, b) {
+  return a + b;
+}
+
+var a = 3;
+function add(b) {
+  return a + b;
+}
+
+var result;
+function add(a, b) {
+  result = a + b;
+  return result;
+}
+
+function add(a, b) {
+  return a + b + new Date().getSeconds();
+}
+
+function changeProp(obj) {
+  return {
+    ...obj,
+    name: 'Jen'
+  }
+  // obj.name = 'Jen';
+  // return obj;
+}
+var startingValue = {
+  name: "Mike",
+  age: 25
+};
+var res = changeProp(startingValue);
+console.log(startingValue);
+console.log(res);
